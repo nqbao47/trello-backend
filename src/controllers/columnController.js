9f93b8c8c65d77a2816595ca45afd4f7f5da0f72
nqbao47 <baonguyengau14@gmail.com>
2024-03-20 08:56:00 +0700

    Implement API to add newColumn and newCard

---
+++ src/controllers/columnController.js
@@ -0,0 +1,21 @@
+/**
+ * BrianDev
+ */
+import { StatusCodes } from 'http-status-codes'
+import { columnService } from '~/services/columnService'
+
+const createNew = async (req, res, next) => {
+  try {
+    // Điều hướng dữ liệu sang tầng Service
+    const createdColumn = await columnService.createNew(req.body)
+
+    // Có kết quả thì trả về Client
+    res.status(StatusCodes.CREATED).json(createdColumn)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const columnController = {
+  createNew
+}
