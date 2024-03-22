f73a019b1351e2b46dc11daa5d19370e933bf09d
nqbao47 <baonguyengau14@gmail.com>
2024-03-22 16:47:34 +0700

    update WHITELIST_DOMAINS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,9 @@
  * BrianDev
  */
 
-export const WHITELIST_DOMAINS = ['http://localhost:5173']
+export const WHITELIST_DOMAINS = [
+  // 'http://localhost:5173'
+]
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
